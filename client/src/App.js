6a3abfbeb0bc5aa5ce2f2d2fbae0133c8912e8c9
Sagar Gupta <sg85207@gmail.com>
2023-01-04 08:25:48 +0530

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="container">
+      <aside className="side-menu">
+        <div className="side-menu-btn">New Chat</div>
+      </aside>
+      <section className="chat-box">
+        <h1>Chat Box</h1>
+      </section>
+    </div>
+  );
+}
+
+export default App;
